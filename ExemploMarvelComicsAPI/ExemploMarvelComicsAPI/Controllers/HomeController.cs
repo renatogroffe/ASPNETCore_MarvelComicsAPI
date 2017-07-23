34be9c130fa8351a5a29e1bfcd89624dbd974a9f
Renato Groffe <renato.groffe@yahoo.com.br>
2017-07-23 17:42:46 -0300

    Consumindo a API da Marvel Comics em .NET Core
    Exemplo de utilização da Marvel Comics API em uma aplicação ASP.NET Core
    1.1 criada no Visual Studio 2017.

---
+++ ExemploMarvelComicsAPI/ExemploMarvelComicsAPI/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ExemploMarvelComicsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(
+            [FromServices]IConfiguration config)
+        {
+            Personagem personagem;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string ts = DateTime.Now.Ticks.ToString();
+                string publicKey = config.GetSection("MarvelComicsAPI:PublicKey").Value;
+                string hash = GerarHash(ts, publicKey,
+                    config.GetSection("MarvelComicsAPI:PrivateKey").Value);
+                
+                HttpResponseMessage response = client.GetAsync(
+                    config.GetSection("MarvelComicsAPI:BaseURL").Value +
+                    $"characters?ts={ts}&apikey={publicKey}&hash={hash}&" +
+                    $"name={Uri.EscapeUriString("Captain America")}").Result;
+
+                response.EnsureSuccessStatusCode();
+                string conteudo =
+                    response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+
+                personagem = new Personagem();
+                personagem.Nome = resultado.data.results[0].name;
+                personagem.Descricao = resultado.data.results[0].description;
+                personagem.UrlImagem = resultado.data.results[0].thumbnail.path + "." +
+                    resultado.data.results[0].thumbnail.extension;
+                personagem.UrlWiki = resultado.data.results[0].urls[1].url;
+
+            }
+
+            return View(personagem);
+        }
+
+        private string GerarHash(
+            string ts, string publicKey, string privateKey)
+        {
+            byte[] bytes =
+                Encoding.UTF8.GetBytes(ts + privateKey + publicKey);
+            var gerador = MD5.Create();
+            byte[] bytesHash = gerador.ComputeHash(bytes);
+            return BitConverter.ToString(bytesHash)
+                .ToLower().Replace("-", String.Empty);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
